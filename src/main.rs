b61378126c5fd6d9c58883d1a09a11508a49dd36
MaFeLP <60669873+MaFeLP@users.noreply.github.com>
2021-11-09 20:12:03 +0100

    Put functions into EventHandler

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,37 @@
-use serenity::async_trait;
-use serenity::client::{Client, Context, EventHandler};
-use serenity::model::channel::Message;
-use serenity::framework::standard::{
-    StandardFramework,
-    CommandResult,
-    macros::{
-        command,
-        group
-    }
-};
-
 use std::env;
 
-#[group]
-#[commands(ping)]
-struct General;
+use serenity::{
+    async_trait,
+    model::{channel::Message, gateway::Ready},
+    prelude::*,
+    utils::MessageBuilder,
+};
 
 struct Handler;
 
 #[async_trait]
-impl EventHandler for Handler {}
+impl EventHandler for Handler {
+    async fn message(&self, _ctx: Context, _new_message: Message) {
+        if _new_message.content.contains("~ping") {
+            if let Err(why) = _new_message.reply(_ctx, "Pong!").await {
+                println!("Error sending message: {:?}", why)
+            };
+
+            return;
+        }
+    }
+
+    async fn ready(&self, _ctx: Context, _data_about_bot: Ready) {
+        println!("Logged in as {}", _data_about_bot.user.name)
+    }
+}
 
 #[tokio::main]
 async fn main() {
-    let framework = StandardFramework::new()
-        .configure(|c| c.prefix("~")) // set the bot's prefix to "~"
-        .group(&GENERAL_GROUP);
-
     // Login with a bot token from the environment
     let token = env::var("DISCORD_TOKEN").expect("token");
     let mut client = Client::builder(token)
         .event_handler(Handler)
-        .framework(framework)
         .await
         .expect("Error creating client");
 
@@ -39,11 +39,4 @@ async fn main() {
     if let Err(why) = client.start().await {
         println!("An error occurred while running the client: {:?}", why);
     }
-}
-
-#[command]
-async fn ping(ctx: &Context, msg: &Message) -> CommandResult {
-    msg.reply(ctx, "Pong!").await?;
-
-    Ok(())
 }
