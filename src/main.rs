f441485628589d9b6e9c55adc041a08360fbf1ca
MaFeLP <60669873+MaFeLP@users.noreply.github.com>
2021-11-09 22:07:13 +0100

    Added Känguru Knecht bot
    Signed-off-by: MaFeLP <60669873+MaFeLP@users.noreply.github.com>

--- src/main.rs
+++ src/main.rs
@@ -1,59 +1,119 @@
 use std::env;
+use tokio::runtime::Runtime;
 
 use serenity::{
     async_trait,
     model::{channel::Message, gateway::Ready, Permissions},
     prelude::*,
-    utils::MessageBuilder,
+//    utils::MessageBuilder,
 };
 
-struct Handler;
+struct XDHandler;
+struct KaenguruHandler;
 
 #[async_trait]
-impl EventHandler for Handler {
+impl EventHandler for XDHandler {
     async fn message(&self, _ctx: Context, mut _new_message: Message) {
         if _new_message.author.bot {
             return;
         }
 
-        let lower_case = _new_message.content.to_lowercase();
-        if lower_case.contains("xd") {
-            if let Err(why) = _new_message.reply(_ctx, "XDDDDDDD").await {
-                println!("Error sending message: {:?}", why)
+        if _new_message.content.to_lowercase().contains("xd") {
+            match _new_message.reply(_ctx, "XDDDDDDD").await {
+                Ok(msg) => println!("[XD]:\tSend message \"XDDDDDDD\" to channel {}", msg.channel_id),
+                Err(why) => println!("[XD]:\tError sending message: {:?}", why)
             };
-
-            return;
         }
     }
 
     async fn ready(&self, _ctx: Context, _data_about_bot: Ready) {
-        println!("Logged in as {}", _data_about_bot.user.name);
+        println!("[XD]:\tLogged in as {}", _data_about_bot.user.name);
 
         let permissions = Permissions::READ_MESSAGES | Permissions::SEND_MESSAGES | Permissions::EMBED_LINKS;
         match _data_about_bot.user.invite_url(&_ctx, permissions).await {
             Ok(url) => {
-                println!("Bot invitation url is: {}", url);
+                println!("[XD]:\tBot invitation url is: {}", url);
                 return;
             }
             Err(why) => {
-                println!("Error getting invite url: {}", why);
+                println!("[XD:]\tError getting invite url: {}", why);
                 return;
             }
         };
     }
 }
 
-#[tokio::main]
-async fn main() {
-    // Login with a bot token from the environment
-    let token = env::var("DISCORD_TOKEN").expect("token");
-    let mut client = Client::builder(token)
-        .event_handler(Handler)
-        .await
-        .expect("Error creating client");
+#[async_trait]
+impl EventHandler for KaenguruHandler {
+    async fn message(&self, _ctx: Context, mut _new_message: Message) {
+        if _new_message.author.bot {
+            return;
+        }
 
-    // start listening for events by starting a single shard
-    if let Err(why) = client.start().await {
-        println!("An error occurred while running the client: {:?}", why);
+        if _new_message.content.to_lowercase().contains("€") ||
+            _new_message.content.to_lowercase().contains("eur") {
+            match _new_message
+                .channel_id
+                .send_message(&_ctx.http, |m| {
+                    m.embed(|e| {
+                        //e.author();
+                        e.description("So viele Euros???");
+
+                        e
+                    });
+
+                    m
+                }).await {
+                    Ok(msg) => println!("[KG]:\tSending money Message to {}", msg.channel_id),
+                    Err(why) => println!("[KG]:\tError sending message: {:?}", why),
+            };
+        }
     }
+
+    async fn ready(&self, _ctx: Context, _data_about_bot: Ready) {
+        println!("[KG]:\tLogged in as {}", _data_about_bot.user.name);
+
+        let permissions = Permissions::READ_MESSAGES | Permissions::SEND_MESSAGES | Permissions::EMBED_LINKS;
+        match _data_about_bot.user.invite_url(&_ctx, permissions).await {
+            Ok(url) => println!("[KG]:\tBot invitation url is: {}", url),
+            Err(why) => println!("[KG]:\tError getting invite url: {}", why),
+        };
+    }
+}
+
+async fn start_xd() {
+    let xd_token = env::var("DISCORD_TOKEN_XD").expect("xd_token");
+    let mut xd_client = Client::builder(xd_token)
+        .event_handler(XDHandler)
+        .await
+        .expect("[XD]:\tError creating client");
+
+    if let Err(why) = xd_client.start().await {
+        println!("[KG]:\tAn error occurred while running the client: {:?}", why);
+    }
+}
+
+async fn start_kg() {
+    let kg_token = env::var("DISCORD_TOKEN_KAENGURU").expect("kg_token");
+    let mut kg_client = Client::builder(kg_token)
+        .event_handler(KaenguruHandler)
+        .await
+        .expect("[KG]:\tError creating client");
+
+    if let Err(why) = kg_client.start().await {
+        println!("[KG]:\tAn error occurred while running the client: {:?}", why);
+    }
+}
+
+fn main() {
+    println!("[MAIN]:\tStarting \"Känguru Rechenkencht\" and \"XD-Bot\"...");
+    let rt = Runtime::new().unwrap();
+    rt.block_on(async move {
+        tokio::spawn(async { start_xd().await});
+        println!("[ASYN]:\tStarted \"XD-Bot\"!");
+        tokio::spawn(async { start_kg().await});
+        println!("[ASYN]:\tStarted \"Känguru Rechenknecht\"!");
+    });
+    println!("[MAIN]:\tStarted two bots.\n[MAIN]:\tThey should appear in you list shortly!");
+    loop {}
 }
