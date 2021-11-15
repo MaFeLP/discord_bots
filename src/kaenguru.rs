564c1508e9543dfd79247fbe526af8b28a90451e
MaFeLP <60669873+MaFeLP@users.noreply.github.com>
2021-11-15 18:16:46 +0100

    Started implementing the Känguru Knecht
    Added calculating EUR to DM and sending message.
    
    Signed-off-by: MaFeLP <60669873+MaFeLP@users.noreply.github.com>

--- src/kaenguru.rs
+++ src/kaenguru.rs
@@ -15,12 +15,50 @@ impl EventHandler for KaenguruHandler {
 
         if _new_message.content.to_lowercase().contains("€") ||
             _new_message.content.to_lowercase().contains("eur") {
+
+            let mut number: u64 = 0;
+            {
+                let mut tens: u64 = 1;
+                let mut in_euro = false;
+                let mut first_space = true;
+                for c in _new_message.content.to_lowercase().chars().rev() {
+                    if c == '€' {
+                        in_euro = true;
+                    }
+                    if in_euro {
+                        if c == ' ' {
+                            if first_space {
+                                first_space = false;
+                            } else {
+                                in_euro = false;
+                            }
+                            continue
+                        } else {
+                            for n in 0..9 {
+                                if n.to_string() == c.to_string() {
+                                    number += n * tens;
+                                    tens *= 10;
+                                    continue;
+                                }
+                                //dbg!("Found unknown character: {}", c)
+                            }
+                        }
+                    }
+                }
+            }
+
+            let description = format!("{} Euro? Das, das sind ja {} Mark! {} Ostmark! {} Ostmark aufm Schwarzmarkt!", number, number * 2, number *4, number *8);
+
             match _new_message
                 .channel_id
                 .send_message(&_ctx.http, |m| {
                     m.embed(|e| {
                         //e.author();
-                        e.description("So viele Euros???");
+                        e.description(&description);
+                        e.footer(|f| {
+                            f.text("War ich ein guter Rechenknecht?");
+                            f
+                        });
 
                         e
                     });
